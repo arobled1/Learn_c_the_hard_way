511b8a8e6506cceca23214ba064ad53533474df0
Alan Robledo <ar6138@nyu.edu>
2019-09-11 14:00:27 -0400

    added first exercise

---
+++ ex1.c
@@ -0,0 +1,13 @@
+// stdio.h is the header file for standard input and output
+#include <stdio.h>
+
+/* This is a comment. */
+int main(int argc, char *argv[])
+{
+	int distance = 100;
+
+	// this is also a comment
+	printf("You are %d miles away.\n", distance);
+
+	return 0;
+}
