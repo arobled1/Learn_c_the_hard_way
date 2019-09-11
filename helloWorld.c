c1cfcbae2292a1aaf8b503f25bd06c873058a281
Alan Robledo <ar6138@nyu.edu>
2019-09-11 13:45:24 -0400

    added first prog

---
+++ helloWorld.c
@@ -0,0 +1,9 @@
+/* Compile with "make helloWorld" to get an executable with the same name */
+/* Compile with "gcc helloWorld.c" to get an executable called a.out */
+# include <stdio.h>
+/* You start the block of the main function with a { and end it with a } */
+/* Why "int" ??? */
+  int main(){
+  printf("Hello World!\n");
+  return 0;
+}
